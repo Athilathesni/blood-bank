1344c626d02a551770d7d4d9dc9273cbf7037706
Athilathesni <usmanchusman606@gmail.com>
2024-09-30 13:05:53 +0530

    file added

--- blood-bank/serverside/server.js
+++ blood-bank/serverside/server.js
@@ -1,20 +1,35 @@
 const http=require ("http")
 const fs=require ("fs")
 const url=require ("url")
-const { prototype } = require("events")
-const PORT=3000
+const PORT=3000;
 const app=http.createServer(async(req,res)=>{
     const path=url.parse(req.url)
-    console.log(path);
+    // console.log(path);
 
 
     if(path.pathname="/"){
         res.writeHead(200,{"content-type":"text/html"});
         res.end(fs.readFileSync("../clientside/pages/index.html"))
     }
-    if(path.pathname="/clientside/css/inde.css"){
+    else if(path.pathname=='/css/index.css'){
         res.writeHead(200,{"content-type":"text/css"});
         res.end(fs.readFileSync("../clientside/css/index.css"))
+    }
+    else if(path.pathname=='/add'){
+        res.writeHead(200,{"content-type":"text/html"});
+        res.end(fs.readFileSync("../clientside/pages/add.html"))
+    }
+    else if(path.pathname=='/css/add.css'){
+        res.writeHead(200,{"content-type":"text/css"});
+        res.end(fs.readFileSync("../clientside/css/add.css"))
+    }
+    else if(path.pathname=='/clientside/js/custom.js'){
+        res.writeHead(200,{"content-type":"text/js"});
+        res.end(fs.readFileSync("../clientside/js/custome.js"))
+    }
+    else if(path.pathname=='/clientside/js/add.js'){
+        res.writeHead(200,{"content-type":"text/js"});
+        res.end(fs.readFileSync("../clientside/js/add.js"))
     }
 })
 app.listen(PORT)
