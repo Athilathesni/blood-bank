47b3f61d81e79dd8732ce4234d7b378b767388e7
Athilathesni <usmanchusman606@gmail.com>
2024-10-02 11:27:01 +0530

    file added

--- serverside/server.js
+++ serverside/server.js
@@ -7,7 +7,7 @@ const{MongoClient, ObjectId}=require("mongodb")
 // const {error}=require("console")
 //connect mongodb
 const client=new MongoClient("mongodb://127.0.0.1:27017/")
-const PORT=4000;
+const PORT=3000;
 
 const app=http.createServer(async(req,res)=>{
 
@@ -21,28 +21,28 @@ const path=url.parse(req.url)
 console.log(path);
 
 if(path.pathname=="/"){
-    res.writeHead(200,{"content-type":"text/html"})
+    res.writeHead(200,{"Content-Type":"text/html"})
     res.end(fs.readFileSync("../clientside/pages/index.html"))
 }
 
-else if(path.pathname=='/css/index.css'){
-    res.writeHead(200,{"content-type":"text/css"})
+else if(path.pathname=="/css/index.css"){
+    res.writeHead(200,{"Content-Type":"text/css"})
     res.end(fs.readFileSync("../clientside/css/index.css"))
 }
-else if(path.pathname=='/add'){
-    res.writeHead(200,{"content-type":"text/html"})
+else if(path.pathname=="/add"){
+    res.writeHead(200,{"Content-Type":"text/html"})
     res.end(fs.readFileSync("../clientside/pages/add.html"))
 }
-else if(path.pathname=='/css/add.css'){
-    res.writeHead(200,{"content-type":"text/css"})
+else if(path.pathname=="/css/add.css"){
+    res.writeHead(200,{"Content-Type":"text/css"})
     res.end(fs.readFileSync("../clientside/css/add.css"))
 }
-// else if(path.pathname=='/js/custome.js'){
-//     res.writeHead(200,{"content-type":"text/js"})
-//     res.end(fs.readFileSync("../clientside/js/custome.js"))
-// }
-else if(path.pathname=='/js/index.js'){
-    res.writeHead(200,{"content-type":"text/js"})
+else if(path.pathname=="/js/add.js"){
+    res.writeHead(200,{"Content-Type":"text/js"})
+    res.end(fs.readFileSync("../clientside/js/add.js"))
+}
+else if(path.pathname=="/js/index.js"){
+    res.writeHead(200,{"Content-Type":"text/js"})
     res.end(fs.readFileSync("../clientside/js/index.js"))
 }
  else if(path.pathname=="/submit"&&req.method=="POST"){
@@ -63,24 +63,24 @@ else if(path.pathname=='/js/index.js'){
             }).catch((error)=>{
                 console.log(error); 
             })
-            res.writeHead(200,{"content-type":"text/html"})
+            res.writeHead(200,{"Content-Type":"text/html"})
             res.end(fs.readFileSync("../clientside/pages/index.html")) 
         }
     })
 }
 
 //get doner
-if(path.pathname=='/getdoner'&&req.method=='GET'){
+if(path.pathname=="/getdoner"&&req.method=="GET"){
     const data=await collection.find().toArray();
     const json_data=JSON.stringify(data)
     console.log(json_data);
-    res.writeHead(200,{"content-type":"text/json"})
+    res.writeHead(200,{"Content-Type":"text/json"})
     res.end(json_data)  
 }
 
-else if(path.pathname=='/delete'&& req.method=='DELETE'){
+else if(path.pathname=="/Delete"&& req.method=="DELETE"){
     console.log("...............delete..............");
-    let body=''
+    let body=""
     req.on('data',(chunks)=>{
         body+=chunks.toString()
         console.log(body);
@@ -88,12 +88,12 @@ else if(path.pathname=='/delete'&& req.method=='DELETE'){
 
 req.on('end',async()=>{
     let_id=new ObjectId(body)
-    console.log(_id);
+    console.log(id);
     await collection.deleteOne({_id}).then(()=>{
-        res.writeHead(200,{"content-type":"text/plain"})
+        res.writeHead(200,{"Content-Type":"text/plain"})
         res.end("success")
     }).catch(()=>{
-        res.writeHead(200,{"content-type":"text/plain"})
+        res.writeHead(200,{"Content-Type":"text/plain"})
         res.end("failed")
     })  
 })
@@ -112,12 +112,12 @@ req.on('end',async()=>{
     let updateData={name:data.name,email:data.email,phone:data.phone,blood:data.blood,gender:data.gender}
     await collection.updateOne({_id},{$set:updateData}).then(()=>{
         console.log("update successfully");
-        res.writeHead(200,{"content-type":"text/plain"})
+        res.writeHead(200,{"Content-Type":"text/plain"})
         res.end("success")
         
     }).catch((error)=>{
         console.log(error);
-        res.writeHead(200,{"content-type":"text/plain"})
+        res.writeHead(200,{"Content-Type":"text/plain"})
         res.end("failed")
     })
     
